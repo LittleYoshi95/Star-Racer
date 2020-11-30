c59d69b03da0a79a1ab77488c8807d792c5f78a6
yocho <carcorjorge@gmail.com>
2020-11-30 09:38:15 -0600

    Changed some setting to deploy to Heroku

--- gulpfile.js
+++ gulpfile.js
@@ -102,7 +102,7 @@ io.on("connection", function (socket) {
 	});
 });
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.PORT || 8080;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 /*
